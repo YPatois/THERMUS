8745ec881a9f507e76addfc234164058e2b7b8ba
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-10-29 10:25:39 +0100

    Way more complete tests mechanism

--- tests/inplace_test.sh
+++ tests/inplace_test.sh
@@ -6,15 +6,56 @@ RUN_THERMUS=$1
 # Location of test files
 TESTDIR=`dirname -- "$( readlink -f -- "$0"; )"`
 
+# Remove old tests resulst
+rm -f tests/results/*.txt
+
+# Run test
 # $THERMUS is not yet defined
 # So the argument is single-quoted to avoid expansion
-$RUN_THERMUS '$THERMUS/share/doc/Thermus/tests/prediction.C -b -q' > brut_result.txt
+$RUN_THERMUS '$THERMUS/share/doc/Thermus/tests/all_predictions.C -b -q'
+$RUN_THERMUS '$THERMUS/share/doc/Thermus/tests/lhc5020_fit_charm.C -b -q'
 
-sed -n '/predicted values/,$p' brut_result.txt > result.txt
+# If there are no results files, test failed
+if compgen -G "tests/results/*.txt" > /dev/null; then
+    echo "Some results files were found"
+else
+    echo "TEST FAILED: no results files were found"
+    exit 1
+fi
 
-diff result.txt $TESTDIR/prediction_expected.txt
+# We need the THERMUS variable for the rest of the script
+`$RUN_THERMUS --getenv`
 
-if [ $? -ne 0 ]; then
+# Number of test files
+nbf=`compgen -G "tests/results/*.txt" | wc -l`
+
+# Number of expected files
+nbe=`compgen -G "$THERMUS/share/doc/Thermus/tests/expected_results/*.txt" | wc -l`
+
+# All tests will be performed, even if some failed, this is to ensure better debugging
+NBF_FAILED=0
+if [ $nbf -ne $nbe ]; then
+  echo "TEST FAILED: number of expected files does not match number of results files"
+  NBF_FAILED=1
+fi
+
+RES_FAILED=0
+# For each file in the result directory, test if it's same as in the expected directory
+for ff in `ls tests/results/*.txt`; do
+  f=`basename $ff`
+  diff tests/results/$f $THERMUS/share/doc/Thermus/tests/expected_results/$f
+  if [ $? -ne 0 ]; then
+    echo "TEST FAILED for file $f"
+    RES_FAILED=1
+  fi
+done
+
+if [ $NBF_FAILED -eq 1 ] || [ $RES_FAILED -eq 1 ]; then
+  env
+  root --version
+  cc --version
+  cpp --version
+
   echo "TEST FAILED"
   exit 1
 fi
