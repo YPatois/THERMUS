fe239183e64eb541c2e57a1210c29312830faa7b
yannick.patois@iphc.cnrs.fr <Yannick Patois>
2025-11-27 17:03:39 +0100

    Let root finds out the platform extension for libs

--- scripts/use_thermus.C
+++ scripts/use_thermus.C
@@ -1,8 +1,8 @@
 // Run this code to include Thermus in your ROOT session
 R__ADD_INCLUDE_PATH($THERMUS/include/Thermus)
 R__ADD_LIBRARY_PATH($THERMUS_LIB)
-R__LOAD_LIBRARY(libFunctions.so)
-R__LOAD_LIBRARY(libTHERMUS.so)
+R__LOAD_LIBRARY(libFunctions)
+R__LOAD_LIBRARY(libTHERMUS)
 
 TString THERMUS = gSystem->Getenv("THERMUS");
 TString THERMUS_LIB = gSystem->Getenv("THERMUS_LIB");
